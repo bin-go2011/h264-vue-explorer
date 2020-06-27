a27921de97617041d587ed269e32f728ec32c00a
ben <bin.go2011@yahoo.com>
2020-06-27 23:15:56 +0800

    use c struct as type directly in go struct

--- backend/h264bitstream/h264_stream.go
+++ backend/h264bitstream/h264_stream.go
@@ -2,19 +2,17 @@ package h264bitstream
 
 //#include "h264_stream.h"
 import "C"
-import "unsafe"
 
 type h264Stream struct {
-	handle uintptr
+	handle *C.h264_stream_t
 }
 
 func NewStream() *h264Stream {
 	return &h264Stream{
-		handle: uintptr(unsafe.Pointer(C.h264_new())),
+		handle: C.h264_new(),
 	}
 }
 
 func (s *h264Stream) Release() {
-	h := (*C.h264_stream_t)(unsafe.Pointer(s.handle))
-	C.h264_free(h)
+	C.h264_free(s.handle)
 }
